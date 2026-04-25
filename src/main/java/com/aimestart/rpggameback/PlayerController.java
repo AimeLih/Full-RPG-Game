c72845633d82ae193e0a64404bcdb3048e66e53b
AimeLih <aimelihono@gmail.com>
2026-04-25 12:10:18 -0500

    Enhanced battle mechanics: refined item usage and attack logic, added critical hit messages, enemy counterattacks, and dynamic battle responses.

--- src/main/java/com/aimestart/rpggameback/PlayerController.java
+++ src/main/java/com/aimestart/rpggameback/PlayerController.java
@@ -45,9 +45,35 @@ public class PlayerController {
     }
 
     @PostMapping("/itemuse/{item}")
-    public GameStateResponse UseItem(@PathVariable String item){
+    public BattleResponse UseItem(@PathVariable String item){
         player1.backpack.put(item, player1.backpack.get(item) - 1);
-        return new GameStateResponse(player1);
+
+        String useMessage;
+
+        switch (item) {
+            case "Health Potion":
+                player1.setHp(player1.getHp() + 20);
+                useMessage = "You drink the potion. +20 HP!";
+                break;
+            default:
+                useMessage = "You used " + item + ".";
+        }
+
+        currentenemy.checkEnemy();
+        int enemyDmg = currentenemy.getAtk();
+        player1.setHp(player1.getHp() - enemyDmg);
+        useMessage += " Enemy strikes back for " + enemyDmg + "!";
+
+        return new BattleResponse(
+                currentenemy.getHp(),
+                0,
+                enemyDmg,
+                currentenemy.getDescription(),
+                player1.getHp(),
+                currentenemy.getName(),
+                useMessage,
+                ""
+        );
     }
 
     @PostMapping("/battle/start")
@@ -57,60 +83,74 @@ public class PlayerController {
                 currentenemy.getHp(),
                 0,
                 0,
+                currentenemy.getDescription(),
                 player1.getHp(),
                 currentenemy.getName(),
-                "Enemy Created"
+                "Enemy Created",
+                ""
         );
     }
     @PostMapping("/battle/attack")
-    public BattleResponse attack(){
+    public BattleResponse attack() {
         player1.check();
-        int dmg = player1.getAtk();
-
-        int critcheck = rand.nextInt(101);
-        boolean critornaw = critcheck <= player1.getCrit();
-        if (critornaw) {
-            dmg *= 2;
-        }
-
-
+        int damage = player1.getAtk();
         int reduction = 0;
+        boolean crit = false;
+
+        Random rand = new Random();
+        int critcheck = rand.nextInt(100) + 1;
+        if (critcheck <= player1.getCrit()) {
+            damage *= 2;
+            crit = true;
+        }
+
         if (currentenemy.getDef() != 0) {
-            reduction = (currentenemy.getDef() / 100) * dmg;
+            reduction = (currentenemy.getDef() / 100) * player1.getAtk();
         }
-        currentenemy.setHp(currentenemy.getHp() - (dmg - reduction));
 
-        String message = "Continue";
+        currentenemy.setHp(currentenemy.getHp() - (damage - reduction));
+
+        String hitMessage = "";
+        if(crit){
+            hitMessage = "Your " + player1.getWeapon() + " feels light — CRITICAL HIT for " + damage + " damage!";
+        } else {
+          hitMessage =  "You strike with your " + player1.getWeapon() + " for " + damage + " damage.";
+        }
 
 
-        if(currentenemy.getHp() <= 0){
-            currentenemy.death(player1); // Award XP
-            player1.setGold(player1.getGold() + currentenemy.getGold());
-            if(player1.getXpbar() >= player1.getEndbar()){
-                player1.levelup();
+        String finalMessage;
+        String counterMessage;
+        int enemyDmg = 0;
+
+        if (currentenemy.getHp() <= 0) {
+            finalMessage   = "Victory! " + currentenemy.getName() + " has fallen.";
+            counterMessage = "";
+        } else {
+            // Enemy only counters if still alive
+            currentenemy.checkEnemy();
+             enemyDmg = currentenemy.getAtk();
+            player1.setHp(player1.getHp() - enemyDmg);
+
+            finalMessage = hitMessage;
+
+            if (player1.getHp() <= 0) {
+                counterMessage = "Darkness consumes you...";
+            } else if (player1.getHp() <= 15) {
+                counterMessage = currentenemy.getName() + " retaliates for " + enemyDmg + ". You are barely standing!";
+            } else {
+                counterMessage = currentenemy.getName() + " strikes back for " + enemyDmg + " damage.";
             }
-            message = "Victory! Gained " + currentenemy.getXp() + " XP and " + currentenemy.getGold() + " gold";
-            return new BattleResponse(0, dmg - reduction, 0, player1.getHp(),  currentenemy.getName(), message);
-        }
-
-
-        currentenemy.checkEnemy();
-        int edmg = currentenemy.getAtk();
-        player1.setHp(player1.getHp() - edmg);
-
-
-        if(player1.getHp() <= 0){
-            message = "Game Over";
         }
 
         return new BattleResponse(
                 currentenemy.getHp(),
-                dmg - reduction,
-                edmg,
+                damage,
+                enemyDmg,
+                currentenemy.getDescription(),
                 player1.getHp(),
-
                 currentenemy.getName(),
-                message
+                finalMessage,
+                counterMessage
         );
     }
 
